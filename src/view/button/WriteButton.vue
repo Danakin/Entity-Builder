166a051caf0d22ccb68b279b6f3fc6b97ffe0833
GooGee <mou-gj@live.com>
2020-10-21 16:35:20 +0800

    + deploy in WriteButton.vue

--- src/view/button/WriteButton.vue
+++ src/view/button/WriteButton.vue
@@ -1,8 +1,9 @@
 <template>
-    <b-button @click="render" variant="outline-primary"> ▼ </b-button>
+    <b-button @click="render" variant="outline-success"> ▼ </b-button>
 </template>
 
 <script>
+import { isConnected, deploy, download, getErrorMessage } from '@/request.js'
 import sss from '@/state.js'
 
 export default {
@@ -29,10 +30,14 @@ export default {
                     }
                     entity = sss.project.entityManager.make(name)
                 }
-                const file = this.layer.getFullPath(sss.project, entity)
-                // const text = sss.render(this.layer, this.entity)
-                console.log(file)
-                // console.log(text)
+                const text = sss.render(this.layer, entity)
+                if (isConnected()) {
+                    const file = this.layer.getFullPath(sss.project, entity)
+                    this.deploy(file, text)
+                } else {
+                    const file = this.layer.getFileName(entity)
+                    download(file, text)
+                }
             } catch (error) {
                 this.$bvToast.toast(error.message, {
                     title: 'i',
@@ -40,6 +45,27 @@ export default {
                     solid: true,
                 })
             }
+        },
+        deploy(file, text) {
+            const data = {
+                [file]: text,
+            }
+            deploy(data)
+                .then(response => {
+                    this.$bvToast.toast('File deployed', {
+                        title: 'i',
+                        variant: 'success',
+                        solid: true,
+                    })
+                })
+                .catch(error => {
+                    const message = getErrorMessage(error)
+                    this.$bvToast.toast(message, {
+                        title: 'i',
+                        variant: 'danger',
+                        solid: true,
+                    })
+                })
         },
     },
 }
