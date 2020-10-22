a9e60f896bba55e43187d9dae6e6eebf5287dbf8
GooGee <mou-gj@live.com>
2020-10-22 16:43:51 +0800

    -+ LayerProperty.vue

--- src/view/schema/LayerProperty.vue
+++ src/view/schema/LayerProperty.vue
@@ -10,6 +10,7 @@
             </tr>
         </thead>
         <tbody>
+            <slot></slot>
             <tr>
                 <td class="text-right">single</td>
                 <td>
@@ -39,6 +40,12 @@
                 <td class="text-right">path</td>
                 <td>
                     <b-form-input v-model="item.pathPattern"></b-form-input>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">description</td>
+                <td>
+                    <b-form-textarea v-model="item.description" rows="11"></b-form-textarea>
                 </td>
             </tr>
         </tbody>
